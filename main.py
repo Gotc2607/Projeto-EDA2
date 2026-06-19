a2eca339689303c7ffe58c9f9237daeb0ad08bea
Gotc2607 <giovanicoelho2005@gmail.com>
2026-06-19 10:49:03 -0300

    feat: colocando uma estrutura básica no arquivo main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# No arquivo main.py
+from src.estruturas.grafo import GrafoNoticias
+from src.processamento.pln import processar_textos
+from src.algoritmos.kruskal import aplicar_kruskal
+
+# 1. Pessoa 5 lê o JSON
+# 2. Pessoa 2 limpa os textos e calcula as distâncias
+# 3. Pessoa 1 e 4 alimentam o Grafo
+# 4. Pessoa 3 roda o algoritmo
